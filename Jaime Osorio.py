40e1d3e239aad3f6ea8d8679b562d95bfeceb77b
Lortid <jaosoriogo@unal.edu.co>
2024-01-16 11:26:40 -0500

    Dandole espacio a los mensajes con strings

--- Jaime Osorio.py
+++ Jaime Osorio.py
@@ -1,13 +1,13 @@
-a = input("Ingresa tu nombre :")
-b = input("Ingresa lo que estudias:")
+a = input(" Ingresa tu nombre :")
+b = input(" Ingresa lo que estudias :")
 
-if a.lower() == "jaime osorio" and b.lower() == "ingeniería de sistemas":
-    print("Eres Jaime Osorio")
-    print("Programa finalizado, se ha verificado su identidad, eres Jaime GOD")
+if a.lower() == " jaime osorio " and b.lower() == " ingeniería de sistemas ":
+    print(" Eres Jaime Osorio ")
+    print(" Programa finalizado, se ha verificado su identidad, eres Jaime GOD ")
 
 else:
-    print("No eres Jaime Osorio")
+    print(" No eres Jaime Osorio ")
 
     for i in range(0,5):
-        print("Por favor ingresa el nombre correcto, si no eres Jaime no tengas en cuenta esto y no ejecutes más el programa")
+        print(" Por favor ingresa el nombre correcto, si no eres Jaime no tengas en cuenta esto y no ejecutes más el programa ")
     
